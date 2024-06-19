a9de15c9d0affacfb6302bb592ff668bf7f84fe0
gonza <gonza@192.168.150.104>
2024-06-19 15:24:20 +0200

    refactorización realizada

--- src/WelcomeScreen.java
+++ src/WelcomeScreen.java
@@ -1,3 +1,4 @@
+
 import java.awt.CardLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -9,53 +10,56 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+public class WelcomeScreen extends JPanel {
 
-public class WelcomeScreen extends JPanel{
-	JLabel title;
-	JButton go;
-	JButton quit;
-	
-	MainWindow mw;
-	
-	public void setTitle(String t){
-		title.setText(t);
-	}
-	
-	public void quitButtonActionListener(){
-		if(JOptionPane.showConfirmDialog(this, "Are you sure?") == JOptionPane.OK_OPTION)
-	        System.exit(0);
-	}
-	
-	public void goButtonActionListener(){
-		mw.showCard("Two");
-	}
- 
-	public WelcomeScreen(MainWindow mw){
-		this.mw = mw;
-		
-		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		title = new JLabel();
-		add(title);
-		
-		//add formatting here
-		
-		go = new JButton("New Game");
-		quit = new JButton("Quit");	
-		
-		go.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				goButtonActionListener();
-			}
-		});
-		
-		quit.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				 quitButtonActionListener();
-			}
-		});
-		
-		add(go);
-		add(quit);
-	}
+    JLabel title;
+    JButton go;
+    JButton quit;
 
+    MainWindow mw;
+
+    public void setTitle(String t) {
+        title.setText(t);
+    }
+
+    public void quitButtonActionListener() {
+        if (JOptionPane.showConfirmDialog(this, "Are you sure?") == JOptionPane.OK_OPTION) {
+            System.exit(0);
+        }
+    }
+
+    public void goButtonActionListener() {
+        mw.showCard("Two");
+    }
+
+    public WelcomeScreen(MainWindow mw) {
+        this.mw = mw;
+
+        final String new_game_button_text = "New Game";
+        final String quit_button_text = "Quit";
+        final int box_layout_axis = BoxLayout.PAGE_AXIS;
+
+        setLayout(new BoxLayout(this, box_layout_axis));
+        title = new JLabel();
+        add(title);
+
+        // add formatting here
+        go = new JButton(new_game_button_text);
+        quit = new JButton(quit_button_text);
+
+        go.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                goButtonActionListener();
+            }
+        });
+
+        quit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                quitButtonActionListener();
+            }
+        });
+
+        add(go);
+        add(quit);
+    }
 }
